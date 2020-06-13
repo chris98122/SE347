88d698520ec39dfef58574d0f9d9e7f8c5dc1dc1
chris98122 <chris98122@126.com>
2020-06-13 18:40:19 +0800

    add deployscript

---
+++ kv-store/src/main/deploy/deployworkers.sh
@@ -0,0 +1,5 @@
+cd worker1
+java -cp  ../kv-store/target/kv-store-1.0-SNAPSHOT.jar Worker  112.124.23.139:2181 212.64.64.185 12200 &
+cd ..
+cd worker2
+java -cp  ../kv-store/target/kv-store-1.0-SNAPSHOT.jar Worker  112.124.23.139:2181 212.64.64.185 12201 &
