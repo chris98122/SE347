8e8cda3c3100152b765b31a6c6585f05d0cc90f7
chris98122 <chris98122@126.com>
2020-06-13 23:29:41 +0800

    deploy.sh

--- kv-store/src/main/deploy/deployworkers.sh
+++ kv-store/src/main/deploy/deployworkers.sh
@@ -1,8 +1,8 @@
 mkdir worker1
 mkdir worker2
 cd worker1
-sudo netstat -tunlp| grep 12200 |grep -v grep| awk  '{print $7}' |cut -d"/" -f1 |sudo xargs kill -9
-sudo netstat -tunlp| grep 12201 |grep -v grep| awk  '{print $7}' |cut -d"/" -f1 |sudo xargs kill -9
+sudo netstat -tunlp| grep 12200 | awk  '{print $7}' |cut -d"/" -f1 |sudo xargs kill -9
+sudo netstat -tunlp| grep 12201 | awk  '{print $7}' |cut -d"/" -f1 |sudo xargs kill -9
 java -cp  ../kv-store/target/kv-store-1.0-SNAPSHOT.jar Worker  112.124.23.139:2181 212.64.64.185 12200 &
 cd ..
 cd worker2
