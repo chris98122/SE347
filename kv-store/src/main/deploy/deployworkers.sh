2a25d19f5d9b19f5c1a5426f282791a3ec513443
chris98122 <chris98122@126.com>
2020-06-13 18:57:38 +0800

    deploy.sh

--- kv-store/src/main/deploy/deployworkers.sh
+++ kv-store/src/main/deploy/deployworkers.sh
@@ -1,7 +1,7 @@
 mkdir worker1
 mkdir worker2
 cd worker1
-java -cp  ./kv-store-1.0-SNAPSHOT.jar Worker  112.124.23.139:2181 212.64.64.185 12200 &
+java -cp  ../kv-store/target/kv-store-1.0-SNAPSHOT.jar Worker  112.124.23.139:2181 212.64.64.185 12200 &
 cd ..
 cd worker2
-java -cp  ./kv-store-1.0-SNAPSHOT.jar Worker  112.124.23.139:2181 212.64.64.185 12201 &
+java -cp  ../kv-store/target/kv-store-1.0-SNAPSHOT.jar Worker  112.124.23.139:2181 212.64.64.185 12201 &
