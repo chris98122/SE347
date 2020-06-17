ebbf8a7fc7eb99b136c1b8f1042265d04f8e24c0
chris98122 <chris98122@126.com>
2020-06-18 07:26:49 +0800

    deploy.sh

--- kv-store/src/main/deploy/deployworkers.sh
+++ kv-store/src/main/deploy/deployworkers.sh
@@ -2,11 +2,11 @@ mkdir worker1
 mkdir worker2
 cd worker1
 sudo rm -rf log4j2.log
-sudo netstat -tunlp| grep 12200 | awk  '{print $7}' |cut -d"/" -f1 |sudo xargs kill -9
 sudo netstat -tunlp| grep 12201 | awk  '{print $7}' |cut -d"/" -f1 |sudo xargs kill -9
-java -cp  ../kv-store/target/kv-store-1.0-SNAPSHOT.jar Worker  112.124.23.139:2181,112.124.23.139:2182,112.124.23.139:2183 212.64.64.185 12200 &
+sudo netstat -tunlp| grep 12202 | awk  '{print $7}' |cut -d"/" -f1 |sudo xargs kill -9
+java -cp  ../kv-store/target/kv-store-1.0-SNAPSHOT.jar Worker  112.124.23.139:2181,112.124.23.139:2182,112.124.23.139:2183 212.64.64.185 12201 &
 cd ..
 cd worker2
 sudo rm -rf log4j2.log
-java -cp  ../kv-store/target/kv-store-1.0-SNAPSHOT.jar Worker  112.124.23.139:2181,112.124.23.139:2182,112.124.23.139:2183 212.64.64.185 12201 &
+java -cp  ../kv-store/target/kv-store-1.0-SNAPSHOT.jar Worker  112.124.23.139:2181,112.124.23.139:2182,112.124.23.139:2183 212.64.64.185 12202 &
 
--- kv-store/src/main/deploy/testScaleOut.sh
+++ kv-store/src/main/deploy/testScaleOut.sh
@@ -1,6 +1,7 @@
 mkdir worker3
 cd worker3
 sudo rm -rf log4j2.log
+sudo netstat -tunlp| grep 12200 | awk  '{print $7}' |cut -d"/" -f1 |sudo xargs kill -9
 sudo netstat -tunlp| grep 12301 | awk  '{print $7}' |cut -d"/" -f1 |sudo xargs kill -9
 sudo netstat -tunlp| grep 12302 | awk  '{print $7}' |cut -d"/" -f1 |sudo xargs kill -9
 java -cp  ../kv-store/target/kv-store-1.0-SNAPSHOT.jar ScaleOutTest
