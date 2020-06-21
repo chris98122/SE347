95710cfedca4e1b39d50dbe3cc726ff1d3b961b0
chris98122 <chris98122@126.com>
2020-06-21 23:52:06 +0800

    catch SofaTimeOutException

--- kv-store/src/main/deploy/testScaleOut.sh
+++ kv-store/src/main/deploy/testScaleOut.sh
@@ -55,17 +55,17 @@ java -cp  ../kv-store/target/kv-store-1.0-SNAPSHOT.jar Worker  112.124.23.139:21
  212.64.64.185 12301  212.64.64.185 12502  & #standyby2 data node
 cd ..
 
-cd worker4-standyby1
-sudo rm -rf log4j2.log
-sudo  find ./ -name 'snapshot*-*' -exec rm {} \;
-java -cp  ../kv-store/target/kv-store-1.0-SNAPSHOT.jar Worker  \
-112.124.23.139:2181,112.124.23.139:2182,112.124.23.139:2183 212.64.64.185 12302 \
-212.64.64.185 12503 & #primary data node
-cd ..
-
-cd worker4-standyby2
-sudo rm -rf log4j2.log
-sudo  find ./ -name 'snapshot*-*' -exec rm {} \;
-java -cp  ../kv-store/target/kv-store-1.0-SNAPSHOT.jar Worker  \
-112.124.23.139:2181,112.124.23.139:2182,112.124.23.139:2183 212.64.64.185 12302 \
-212.64.64.185 12504 & #primary data node
+#cd worker4-standyby1
+#sudo rm -rf log4j2.log
+#sudo  find ./ -name 'snapshot*-*' -exec rm {} \;
+#java -cp  ../kv-store/target/kv-store-1.0-SNAPSHOT.jar Worker  \
+#112.124.23.139:2181,112.124.23.139:2182,112.124.23.139:2183 212.64.64.185 12302 \
+#212.64.64.185 12503 & #primary data node
+#cd ..
+#
+#cd worker4-standyby2
+#sudo rm -rf log4j2.log
+#sudo  find ./ -name 'snapshot*-*' -exec rm {} \;
+#java -cp  ../kv-store/target/kv-store-1.0-SNAPSHOT.jar Worker  \
+#112.124.23.139:2181,112.124.23.139:2182,112.124.23.139:2183 212.64.64.185 12302 \
+#212.64.64.185 12504 & #primary data node
